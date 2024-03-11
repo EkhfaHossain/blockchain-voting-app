00021e87226def571bdcb45405ff6dbb2ffb7832
EkhfaHossain <ekhfa.hossain@sazim.io>
2024-03-11 15:08:48 +0600

    Deployement of smart contract

---
+++ scripts/deploy.ts
@@ -0,0 +1,15 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  const Create = await ethers.getContractFactory("Create");
+
+  const create = await Create.deploy();
+  await create.waitForDeployment();
+
+  console.log("Create contract deployed to:", create.address);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
